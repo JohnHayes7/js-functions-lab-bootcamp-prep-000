77206010e40aa9436922aec44c66fce768e866f9
johnnyhayes7@gmail.com <johnnyhayes7@gmail.com>
2018-09-07 21:57:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,6 @@ function happyHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown(days, holiday)
+function holidayCountdown(days, holiday){
+  return `It's ${days} until ${holiday}`
+}
